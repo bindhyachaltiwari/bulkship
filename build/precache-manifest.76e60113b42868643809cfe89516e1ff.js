c8b21bf12a2e3176fcf5d44bd7ff50198b51c209
Bindhyachal Nath Tiwari <bindhyachal.tiwari@provartesting.com>
2020-02-23 00:28:01 +0530

    files added

---
+++ build/precache-manifest.76e60113b42868643809cfe89516e1ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4543837e3def8b86050501bd02bbf210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b7168ab1795bf4e53a1",
+    "url": "/static/css/2.989a1981.chunk.css"
+  },
+  {
+    "revision": "e4d0c9a12a3a3910569b",
+    "url": "/static/css/main.339f3ac9.chunk.css"
+  },
+  {
+    "revision": "1b7168ab1795bf4e53a1",
+    "url": "/static/js/2.58f97959.chunk.js"
+  },
+  {
+    "revision": "a8b87c0c0ca4afafdce5c588d39a329f",
+    "url": "/static/js/2.58f97959.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4d0c9a12a3a3910569b",
+    "url": "/static/js/main.a1758437.chunk.js"
+  },
+  {
+    "revision": "64ab9c03c0a2dae52e60",
+    "url": "/static/js/runtime-main.7948bca8.js"
+  }
+]);
